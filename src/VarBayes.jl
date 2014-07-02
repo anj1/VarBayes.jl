23d3e994f11290deea1f160ce890f03a5bc2d37f
anj1 <anej001@aucklanduni.ac.nz>
2014-07-02 17:16:30 +1200

    export types

--- src/VarBayes.jl
+++ src/VarBayes.jl
@@ -5,6 +5,7 @@ using Distributions
 import Base: rand
 import Distributions: logpdf
 
+export BayesNormal, BayesCategorical, BayesDiagNormal
 export infer, fit_mm_em, init_mm_var, fit_mm_var!, fit_cimm_var!
 
 include("fit_mm_em.jl")
