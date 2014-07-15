c62e693f5349b242ebfb9ff2a78775127e7c6f78
anj1 <anej001@aucklanduni.ac.nz>
2014-07-15 15:14:39 +1200

    Fixed normalization issue in fit_cimm_var!

--- src/fit_cimm_var.jl
+++ src/fit_cimm_var.jl
@@ -1,8 +1,12 @@
 # Variational estimation for
 # circular shift-invariant Mixture Model (CIMM)
-function logp_to_p_rot(lp)
+
+# take log-probabilities, convert to probabilities and normalize.
+# dims is the set of dimensions that we normalize over.
+# TODO: make this more stable
+function logp_to_p_rot(lp, dims)
 	r = exp(lp .- maximum(lp))
-	r ./ sum(sum(r,3),2)
+	r ./ sum(r,dims)
 end
 
 function fit_cimm_var!(m::MixtureModel, comppri, mixpri, x, rskip)
@@ -12,32 +16,34 @@ function fit_cimm_var!(m::MixtureModel, comppri, mixpri, x, rskip)
 	ncomps = length(m.component)
 
 	# calculate probs and also extend x by adding shifted copies of data
+	# ρ[Input, Component, Rotation]
 	ρ = Array(Float64, size(x,2), ncomps, R)
 	y = Array(Float64, size(x,1), 0)
 	for r = 1 : R
 		shiftx = circshift(x, [rskip*(r-1), 0])
-		ρ[:,:,r] = infer(m, shiftx)
+		ρ[:,:,r] = infer(m, shiftx) 
 		y = cat(2, y, shiftx)
 	end
 
-	s = logp_to_p_rot(ρ)
+	s = logp_to_p_rot(ρ, (2, 3))  # Normalize over component and rotation
 
 	# for each component
 	cr = 1:ncomps
 	for k in cr
 		# Update this component
-		m.component[k] = fit_mleb(comppri, y, reshape(s[:,k,:], (size(x,2)*R,)))
+		# todo: replace /R with something in the calculation of s
+		m.component[k] = fit_mleb(comppri, y, reshape(s[:,k,:], (size(x,2)*R,))/R)
 
 		# update probabilities
 		for r = 1 : R
 			shiftx = circshift(x, [rskip*(r-1), 0])
 			ρ[:,k,r] = logpdf(m.component[k], shiftx) .+ logpdf(m.mixing, k)
 		end
-		s = logp_to_p_rot(ρ)
+		s = logp_to_p_rot(ρ, (2, 3))
 	end
 
 	# Finally, update the mixing parameters
 	# Note that when calculating the mixing parameters, all rotations
 	# of the component are considered to be the same component.
-	m.mixing = fit_mleb(mixpri, [cr], vec(sum(sum(s,1),3)))
+	m.mixing = fit_mleb(mixpri, [cr], vec(sum(s, (1, 3))))
 end
